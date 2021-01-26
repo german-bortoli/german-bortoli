c48c7fb3a6083c6da3b9fbe34a4f9b4fee808700
German Bortoli <german.bortoli@bairesdev.com>
2021-01-26 14:22:59 -0300

    Added GTAG Manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
+import TagManager from 'react-gtm-module'
+import { useEffect } from 'react'
 import '../styles/globals.css'
 
+const tagManagerArgs = {
+  gtmId: 'GTM-T48HMQR',
+  dataLayer: {},
+}
+
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs)
+  }, [])
+
   return <Component {...pageProps} />
 }
 
